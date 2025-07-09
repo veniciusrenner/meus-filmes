3354c605f7c2a319b0dc15f905fce64df7c2340a
VENICIUS RENNER SOUSA BARRETO <veniciusbarreto0755@digitalcollege.internal>
2025-07-08 21:37:29 -0300

    testando api autenticada com sucesso

---
+++ script.js
@@ -0,0 +1,16 @@
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NTQyNmJmOWNhNGQwZDFkZTU4YWYyNGI1YTVkNjFlYSIsIm5iZiI6MTc1MjAxODYzMy41MTksInN1YiI6IjY4NmRhZWM5MjkxMzA3NjU2MzcwM2E1MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zax1XZTWUIoT0b0WJWQ30bhRhw2eME0RT61x9rWry7A'
+    }
+}
+
+
+
+fetch('https://api.themoviedb.org/3/authentication')
+    .then(response => response.json())
+    .then(data =>{
+        console.log(data);
+        // Aqui você pode manipular os dados recebidos da API
+    })
